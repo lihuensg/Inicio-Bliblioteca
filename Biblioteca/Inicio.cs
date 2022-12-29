0ac386e4ec24f3e1f745b22e506399559b95a210
lihuensg <lihuensg@gmail.com>
2022-12-29 16:56:11 -0300

    finalizando registro usuario

--- Biblioteca/Inicio.cs
+++ Biblioteca/Inicio.cs
@@ -13,8 +13,7 @@ namespace Inicio_Bliblioteca
 {
     public partial class Inicio : Form
     {
-        string Usuario = "Pata";
-        string Contraseña = "Pata";
+      
 
         Aplication.Fachada fachada;
 
@@ -41,52 +40,33 @@ namespace Inicio_Bliblioteca
 
         private void botAceptar_Click_1(object sender, EventArgs e)
         {
-            DTOUsuario dTOUsuario1 = new DTOUsuario {
-                Nombre = txtUsuario.Text,
-                Dni = 425,
-                Password = txtContraseña.Text,
-                Mail = "Enzo@gmail.com",
-                FechaRegistro = DateTime.Now,
-                Puntaje = 0,
-            };
 
-            fachada.AgregarUsuario(dTOUsuario1, true);
-
-            if (fachada.LoguearUsuario(dTOUsuario1.Nombre, dTOUsuario1.Password)) {
+            if (fachada.LoguearUsuario(txtUsuario.Text, txtContraseña.Text)) {
                 MessageBox.Show("Logueado correctamente");
-            }
-
-            if (txtUsuario.Text != Usuario || txtContraseña.Text != Contraseña)
-            {
-                if (txtUsuario.Text != Usuario)
-                {
-                    MessageBox.Show("Usuario Incorrecto");
+            
+                    string nombre = txtUsuario.Text;
                     txtUsuario.Clear();
-                    txtUsuario.Focus();
-                    return;
-                }
-                if (txtContraseña.Text != Contraseña)
-                {
-                    MessageBox.Show("Contraseña Incorrecto");
                     txtContraseña.Clear();
-                    txtContraseña.Focus();
-                    return;
-                }
+                    this.Hide();
+                    using (Usuario ventanaUsuario = new Usuario(nombre))
+                        ventanaUsuario.ShowDialog();
             }
             else
             {
-                string nombre = txtUsuario.Text;
-                txtUsuario.Clear();
-                txtContraseña.Clear();
-                this.Hide();               
-                using (Usuario ventanaUsuario = new Usuario(nombre))
-                    ventanaUsuario.ShowDialog();
+               MessageBox.Show("Usuario o Contraseña incorrecta");
             }
+     
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            Registro_Usuario nuevoUsuario = new Registro_Usuario();
+            nuevoUsuario.ShowDialog();
         }
     }
 }
