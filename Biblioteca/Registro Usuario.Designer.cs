6eac3ef6e9f307471b19d94f21db1238341a2a22
Ivan Bonti <ivanbonti300@gmail.com>
2023-02-02 11:53:52 -0300

    permite dni > 100

--- Biblioteca/Registro Usuario.Designer.cs
+++ Biblioteca/Registro Usuario.Designer.cs
@@ -30,18 +30,18 @@ namespace Inicio_Bliblioteca
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.numDNI = new System.Windows.Forms.NumericUpDown();
+            this.texEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.txtContraseña = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.Registro = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.texEmail = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.botCancelar = new System.Windows.Forms.Button();
             this.botAceptar = new System.Windows.Forms.Button();
-            this.numDNI = new System.Windows.Forms.NumericUpDown();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numDNI)).BeginInit();
             this.SuspendLayout();
@@ -57,34 +57,81 @@ namespace Inicio_Bliblioteca
             this.panel1.Controls.Add(this.txtUsuario);
             this.panel1.Controls.Add(this.txtContraseña);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(59, 111);
+            this.panel1.Location = new System.Drawing.Point(88, 171);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(319, 138);
+            this.panel1.Size = new System.Drawing.Size(478, 212);
             this.panel1.TabIndex = 10;
+            // 
+            // numDNI
+            // 
+            this.numDNI.Location = new System.Drawing.Point(222, 109);
+            this.numDNI.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numDNI.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numDNI.Name = "numDNI";
+            this.numDNI.Size = new System.Drawing.Size(180, 26);
+            this.numDNI.TabIndex = 11;
+            // 
+            // texEmail
+            // 
+            this.texEmail.Location = new System.Drawing.Point(222, 162);
+            this.texEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.texEmail.Name = "texEmail";
+            this.texEmail.Size = new System.Drawing.Size(180, 26);
+            this.texEmail.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(117, 168);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(136, 115);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "DNI";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label2.Location = new System.Drawing.Point(65, 12);
+            this.label2.Location = new System.Drawing.Point(98, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.Size = new System.Drawing.Size(86, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Usuario";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(148, 8);
+            this.txtUsuario.Location = new System.Drawing.Point(222, 12);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(121, 20);
+            this.txtUsuario.Size = new System.Drawing.Size(180, 26);
             this.txtUsuario.TabIndex = 4;
             // 
             // txtContraseña
             // 
-            this.txtContraseña.Location = new System.Drawing.Point(148, 39);
+            this.txtContraseña.Location = new System.Drawing.Point(222, 60);
+            this.txtContraseña.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(121, 20);
+            this.txtContraseña.Size = new System.Drawing.Size(180, 26);
             this.txtContraseña.TabIndex = 6;
             this.txtContraseña.UseSystemPasswordChar = true;
             // 
@@ -92,18 +139,20 @@ namespace Inicio_Bliblioteca
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(38, 43);
+            this.label3.Location = new System.Drawing.Point(57, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.Size = new System.Drawing.Size(124, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Contraseña";
             // 
             // Registro
             // 
             this.Registro.AutoSize = true;
-            this.Registro.Location = new System.Drawing.Point(264, 134);
+            this.Registro.Location = new System.Drawing.Point(396, 206);
+            this.Registro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Registro.Name = "Registro";
-            this.Registro.Size = new System.Drawing.Size(0, 13);
+            this.Registro.Size = new System.Drawing.Size(0, 20);
             this.Registro.TabIndex = 9;
             // 
             // label1
@@ -112,47 +161,22 @@ namespace Inicio_Bliblioteca
             this.label1.BackColor = System.Drawing.Color.Silver;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkRed;
-            this.label1.Location = new System.Drawing.Point(136, 61);
+            this.label1.Location = new System.Drawing.Point(204, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.Size = new System.Drawing.Size(246, 29);
             this.label1.TabIndex = 11;
             this.label1.Text = "Registro de Usuario";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(91, 75);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 16);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "DNI";
-            // 
-            // texEmail
-            // 
-            this.texEmail.Location = new System.Drawing.Point(148, 105);
-            this.texEmail.Name = "texEmail";
-            this.texEmail.Size = new System.Drawing.Size(121, 20);
-            this.texEmail.TabIndex = 10;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(78, 109);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 16);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Email";
             // 
             // botCancelar
             // 
             this.botCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.botCancelar.ForeColor = System.Drawing.Color.DarkRed;
-            this.botCancelar.Location = new System.Drawing.Point(230, 270);
+            this.botCancelar.Location = new System.Drawing.Point(345, 415);
+            this.botCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.botCancelar.Name = "botCancelar";
-            this.botCancelar.Size = new System.Drawing.Size(75, 23);
+            this.botCancelar.Size = new System.Drawing.Size(112, 35);
             this.botCancelar.TabIndex = 13;
             this.botCancelar.Text = "Cancelar";
             this.botCancelar.UseVisualStyleBackColor = true;
@@ -162,32 +186,27 @@ namespace Inicio_Bliblioteca
             // 
             this.botAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.botAceptar.ForeColor = System.Drawing.Color.DarkRed;
-            this.botAceptar.Location = new System.Drawing.Point(117, 270);
+            this.botAceptar.Location = new System.Drawing.Point(176, 415);
+            this.botAceptar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.botAceptar.Name = "botAceptar";
-            this.botAceptar.Size = new System.Drawing.Size(75, 23);
+            this.botAceptar.Size = new System.Drawing.Size(112, 35);
             this.botAceptar.TabIndex = 12;
             this.botAceptar.Text = "Aceptar";
             this.botAceptar.UseVisualStyleBackColor = true;
             this.botAceptar.Click += new System.EventHandler(this.botAceptar_Click);
             // 
-            // numDNI
-            // 
-            this.numDNI.Location = new System.Drawing.Point(148, 71);
-            this.numDNI.Name = "numDNI";
-            this.numDNI.Size = new System.Drawing.Size(120, 20);
-            this.numDNI.TabIndex = 11;
-            // 
             // Registro_Usuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(437, 340);
+            this.ClientSize = new System.Drawing.Size(656, 523);
             this.Controls.Add(this.botCancelar);
             this.Controls.Add(this.botAceptar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.Registro);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Registro_Usuario";
             this.Text = "Registro_Usuario";
             this.panel1.ResumeLayout(false);
